93f70b6d881d95ca02caeeabb2653d933b352b95
AlexKi07 <kimanialexk07@gmail.com>
2025-05-08 17:02:08 +0300

    Assign

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    name= "Guido"
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    x =(num1 + num2)
+    return x
+
+add(45, 55)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
